279fe84a2174f9492cff12b1f47aeb0ae509a0b5
مجاهد ‎أمين ‎طوماكن <141775150+mucahid-emin-tomakin@users.noreply.github.com>
2026-06-12 20:51:11 +0200

    [Update] WorkingTimeTracker - MesaiCizelgesi.py

--- Python/WorkingTimeTracker/WorkingTimeTracker - MesaiCizelgesi.py
+++ Python/WorkingTimeTracker/WorkingTimeTracker - MesaiCizelgesi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# WorkingTimeTracker - MesaiCizelgesi
+# WorkingTimeTracker - MesaiCizelgesi.py
 
 # ============================== IMPORTS ==============================
 
